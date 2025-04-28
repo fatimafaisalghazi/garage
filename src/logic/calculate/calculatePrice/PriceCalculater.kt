fa2fd5cb2ba1875a94afed0a7818af37a8f4d2a8
fatima faisal <fatima.faisal.ghazi@gmail.com>
2025-04-28 19:36:29 +0300

    fix constant name

--- src/logic/calculate/calculatePrice/PriceCalculater.kt
+++ src/logic/calculate/calculatePrice/PriceCalculater.kt
@@ -6,12 +6,12 @@ class PriceCalculater(private val distanceCalculator: DistanceCalculator) {
     val name = readFromUser().userinput()
     fun CalculatePrice():Int?{
         val distance= distanceCalculator.getDistanceByDistrict(name)
-          val price=pricePer_KG*(initPrice + distance!!)
+          val price=PRICE_PER_KG*(INITIAL_PRICE + distance!!)
         return price
     }
     companion object{
-        const val pricePer_KG= 10000
-        const val initPrice=5000
+        const val PRICE_PER_KG= 10000
+        const val INITIAL_PRICE=5000
 
     }
 
